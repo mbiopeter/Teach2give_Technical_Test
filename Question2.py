801a5da1025dfcab26244157bfbd32b16a1467e0
mbiopeter <mbiopeter401@gmail.com>
2024-02-07 14:55:27 +0300

    Fibonacci sequence

--- Question2.py
+++ Question2.py
@@ -2,3 +2,18 @@
 Question 2: Fibonacci Sequence
 Write a program to generate the Fibonacci sequence up to 100.
 """
+
+#a Fibonacci sequence is a sequence where the current number is the sum of two preceding numbers
+
+
+def fibonacci():
+    secondPreceding = 0
+    firstPreceding = 1
+    while firstPreceding + secondPreceding <= 100:
+        currentTerm = firstPreceding + secondPreceding
+        print(currentTerm,",")
+        secondPreceding = firstPreceding
+        firstPreceding = currentTerm
+
+fibonacci() 
+
